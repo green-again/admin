a3ff11e014eb0d9792995aec3d5be38099a5ad2b
jmean12 <ooyhy123aa@gmail.com>
2021-12-20 09:50:20 +0900

    Create_admin_Components

--- admin-client/src/pages/Main.tsx
+++ admin-client/src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Main = () => {
-    return (
+    return(
         <div>This is Main!</div>
     );
 };
--- admin-client/src/pages/store/StoreList.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const StoreList = () => {
-    return(
-        <div>This is StoreList!</div>
-    );
-};
-
-export default StoreList;
